6d152648ab970dc41d5da68bd2f0a8abcb2e3a3a
Yash Arora <yasharora2678@gmail.com>
2024-12-15 19:48:57 +0530

    feature: updates env and multer config

--- middlewares/upload.middleware.js
+++ middlewares/upload.middleware.js
@@ -1,17 +1,14 @@
 import multer from "multer";
 
- const upload = multer({
-    storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-           return cb(null, ("uploads"))
-        },
-        filename: (req, file, cb) => {
-           return cb(null, `${Date.now()}_${file.originalname}`)
-        }
- 
-    })
-}).fields([
-   {name: 'image', maxCount: 1}
-]);
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "uploads");
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}_${file.originalname}`);
+    },
+  }),
+}).single("image");
 
 export default upload;
